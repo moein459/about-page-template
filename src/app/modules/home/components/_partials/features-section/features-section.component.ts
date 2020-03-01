05a9a1493fb8c44cec53c4345d09ac76a7702748
moeinZ <moein459@gmail.com>
2020-03-01 16:53:06 +0330

    apply footer and feature section bg animation

--- src/app/modules/home/components/_partials/features-section/features-section.component.ts
+++ src/app/modules/home/components/_partials/features-section/features-section.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import { Observable } from "rxjs";
 import { Feature } from '../../../models/feature.model';
 import { FeatureService } from '../../../services/feature/feature.service';
+import { ScrollService } from 'src/app/modules/core/services/scroll/scroll.service';
 
 @Component({
 	selector: "app-features-section",
@@ -11,7 +12,7 @@ import { FeatureService } from '../../../services/feature/feature.service';
 export class FeaturesSectionComponent implements OnInit {
 	features: Observable<Feature[]>;
 
-	constructor(private _featureService: FeatureService) {}
+	constructor(private _featureService: FeatureService, public scrollService: ScrollService) {}
 
 	ngOnInit(): void {
 		this.features = this._featureService.list();
